7b5a2846b9fd2d7297163283947b06bd0c77d69e
Raj Pandey <rajpandey552002@gmail.com>
2024-06-28 16:26:53 +0530

    Project setup and navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './app.scss'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Skills">Parallax</section>
+      <section>Skills</section>
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contacts">Contact</section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="navbar-name"
+        >
+          RP
+        </motion.span>
+        <div className="social">
+          <a href="https://www.instagram.com/rajpandey._/" target="_blank">
+            <img className="social-icons" src="/instagram.png" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/rajpandey55/" target="_blank">
+            <img className="social-icons" src="/linkedin.png" alt="" />
+          </a>
+          <a href="https://github.com/raj-pandey55" target="_blank">
+            <img className="social-icons" src="/github.png" alt="" />
+          </a>
+          <a href="https://x.com/rajpandeyyyy" target="_blank">
+            <img className="social-icons" src="/twitter.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
