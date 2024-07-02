5f64092b2d5aacc6b1c8269d6bbf7635a2990e9e
Raj Pandey <rajpandey552002@gmail.com>
2024-07-03 04:56:56 +0530

    fixed github see demo button for mobile devices

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import "./portfolio.scss";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform, useAnimation } from "framer-motion";
 import { FaGithub } from "react-icons/fa";
+import {useMediaQuery} from 'react-responsive';
 
 const items = [
   {
@@ -10,7 +11,7 @@ const items = [
     img: "./behance.jpeg",
     desc: "A clone of the Behance website's landing page. This project showcases the implementation of a dynamic landing page with search auto-suggestions and a sorting feature. It aims to replicate the design and functionality of Behance, providing a seamless user experience.",
     url: "https://behance-clone-raj.netlify.app/",
-    github: "https://github.com/raj-pandey55/behance_clone"
+    github: "https://github.com/raj-pandey55/behance_clone",
   },
   {
     id: 2,
@@ -18,7 +19,7 @@ const items = [
     img: "./live-polls.png",
     desc: "A dynamic platform where users can create interactive polls and share them with others. This project was a fantastic opportunity to work with various web technologies and build something that enables real-time interaction and data visualization.",
     url: "https://live-polls-mp0z.onrender.com/",
-    github: "https://github.com/raj-pandey55/Live-Polls"
+    github: "https://github.com/raj-pandey55/Live-Polls",
   },
   {
     id: 3,
@@ -26,7 +27,7 @@ const items = [
     img: "./mapty.jpeg",
     desc: "An interactive platform that allows users to track their workouts, like running and cycling, on a map. It's a simple yet powerful way to visualize fitness activities and stay motivated.",
     url: "https://mapty-raj.netlify.app/",
-    github: "https://github.com/raj-pandey55/Mapty"
+    github: "https://github.com/raj-pandey55/Mapty",
   },
   {
     id: 4,
@@ -34,18 +35,30 @@ const items = [
     img: "./omnifood.jpeg",
     desc: "Omnifood is a landing page of an AI-powered food subscription that will make you healthy again, 365 days per year. It's tailored to your personal tastes and nutritional needs.",
     url: "https://omnifood-rajp.netlify.app/",
-    github: "https://github.com/raj-pandey55/Omnifood"
+    github: "https://github.com/raj-pandey55/Omnifood",
   },
 ];
 
+
+
 const Single = ({ item }) => {
   const ref = useRef();
+  const controls = useAnimation();
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
   const { scrollYProgress } = useScroll({
     target: ref,
   });
 
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  useEffect(() => {
+    if (isMobile) {
+      controls.stop();
+    } else {
+      controls.start({ y: y });
+    }
+  }, [isMobile, controls, y]);
 
   return (
     <section>
@@ -54,15 +67,15 @@ const Single = ({ item }) => {
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt={item.title} />
           </div>
-          <motion.div className="textContainer" style={{ y }}>
+          <motion.div className="textContainer" animate={controls} initial={{ y:1 }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
             <div className="buttons">
-              <a  className="demo" href={item.url} target="_blank">
+              <a className="demo" href={item.url} target="_blank" rel="noopener noreferrer">
                 See Demo
               </a>
-              <a className="github" href={item.github} target="_blank">
-                <FaGithub />
+              <a href={item.github} target="_blank" rel="noopener noreferrer">
+                <FaGithub className="github" />
               </a>
             </div>
           </motion.div>
@@ -71,6 +84,41 @@ const Single = ({ item }) => {
     </section>
   );
 };
+
+
+// const Single = ({ item }) => {
+//   const ref = useRef();
+
+//   const { scrollYProgress } = useScroll({
+//     target: ref,
+//   });
+
+//   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+//   return (
+//     <section>
+//       <div className="container">
+//         <div className="wrapper">
+//           <div className="imageContainer" ref={ref}>
+//             <img src={item.img} alt={item.title} />
+//           </div>
+//           <motion.div className="textContainer" style={{ y }}>
+//             <h2>{item.title}</h2>
+//             <p>{item.desc}</p>
+//             <div className="buttons">
+//               <a className="demo" href={item.url} target="_blank"  rel="noopener noreferrer">
+//                 See Demo
+//               </a>
+//               <a href={item.github} target="_blank"  rel="noopener noreferrer">
+//                 <FaGithub className="github" />
+//               </a>
+//             </div>
+//           </motion.div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
 const Portfolio = () => {
   const ref = useRef();
